6f8a233bea8249bacf5daf948dc204e069752075
Shai <shaibanuelos45@gmail.com>
2025-11-23 14:59:07 -0600

    metodo post

--- practicas/p13/index.php
+++ practicas/p13/index.php
@@ -23,6 +23,17 @@
         return $response; 
     });
 
+    $app->post("/pruebapost", function(Request $request, Response $response, $args){
+        $reqPost = $request->getParsedBody();
+        
+        $val1 = $reqPost["val1"];
+        $val2 = $reqPost["val2"];
+
+        $response->getBody()->write("Valores: " . $val1 . " " . $val2);
+        
+        return $response; 
+    });
+
     $app->run();
 ?>
 
